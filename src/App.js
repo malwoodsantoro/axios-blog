dfe6f4ff422ea94d019298d11559881e45bdeeec
mal-wood <mallorywood413@gmail.com>
2022-03-21 20:56:45 -0700

    clean up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
 
-function App() {
+const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+
+export default function App() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  function updatePost() {
+    axios
+      .put(`${baseURL}`, {
+        title: "Hello World!",
+        body: "This is an updated post."
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
+  }
+
+  function deletePost() {
+    axios
+      .delete(`${baseURL}/1`)
+      .then(() => {
+        alert("Post deleted!");
+        setPost(null)
+      });
+  }
+
+  if (!post) return null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      <button onClick={updatePost}>Update Post</button>
+      <button onClick={deletePost}>Delete Post</button>
     </div>
   );
 }
-
-export default App;
